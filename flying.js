875603cdc2ec612d70083b7cf5488a225dd2c748
dancergraham <graham.knapp@gmail.com>
2021-08-31 08:43:31 +0200

    broken fly list

---
+++ flying.js
@@ -0,0 +1,43 @@
+/**
+ * 
+ * Not currently working properly
+ */
+
+
+
+function checkNoFlyList(passenger) {
+    const NOfLYLIST = ["Dr. Evel"];
+    if (NOfLYLIST.indexOf(passenger) >= 0){
+        return false;
+    }
+    return true;
+}
+function checkNotPaid(passenger) {
+    if (passenger.paid){
+        return false;
+    }
+    return true
+}
+
+function checkPassengers(passengers, functions){
+    var result = [];
+    for (var i = 0; i<passengers.length; i++){
+        var passenger = passengers[i];
+        for (var j = 0; j<functions.length; j++){
+            var f = functions[j]
+            if (!f(passenger)){
+                result.push(false)
+                break;
+            } 
+        }
+        result.push(true)
+    };
+    return result
+}
+
+var passengers = [ { name: "Jane Doloop", paid: true }, { name: "Dr. Evel", paid: true },
+{ name: "Sue Property", paid: false }, { name: "John Funcall", paid: true } ];
+var functions = [checkNoFlyList,
+                 checkNotPaid,
+                ];
+console.log(checkPassengers(passengers, functions));
