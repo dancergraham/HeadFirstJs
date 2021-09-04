d29be667cd2f36fcbda1def0d5ecd0e912733373
dancergraham <graham.knapp@gmail.com>
2021-09-04 09:17:59 +0200

    subclassing with super

--- showdogs.js
+++ showdogs.js
@@ -22,6 +22,27 @@ Dog.prototype.wag = function() {
     console.log("Wag!"); 
 };
 
+class Showdog extends Dog {
+    constructor(name, breed, weight, handler){
+        super(name, breed, weight);
+        this.handler = handler;
+    }
+}
+
+Showdog.prototype.league = "Webville";
+Showdog.prototype.stack = function() {
+    console.log("Stack"); 
+};
+Showdog.prototype.bait = function() {
+    console.log("Bait"); 
+};
+Showdog.prototype.gait = function(kind) {
+    console.log(kind + "ing"); 
+};
+Showdog.prototype.groom = function() {
+    console.log("Groom"); 
+};
+
 function init(){
     var fido = new Dog("Fido", "Mixed", 38);
     var fluffy = new Dog("Fluffy", "Poodle", 30); 
@@ -29,6 +50,10 @@ function init(){
     fido.bark(); fido.run(); fido.wag();
     fluffy.bark(); fluffy.run(); fluffy.wag();
     spot.bark(); spot.run(); spot.wag();;
-    }
+    var scotty = new Showdog("Scotty", "Scottish Terrier", 15, "Cookie");
+    scotty.stack(); scotty.bark(); console.log(scotty.league); console.log(scotty.species);
+    console.log("Fido constructor is " + fido.constructor); 
+    console.log("Scotty constructor is " + scotty.constructor);
+}
 
 window.onload = init();
